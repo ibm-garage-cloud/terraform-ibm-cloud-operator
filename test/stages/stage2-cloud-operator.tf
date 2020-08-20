18f9d32e046f3bc087b3c0c9a1c2d9b7a84a4f4f
Sean Sundberg <seansund@us.ibm.com>
2020-08-20 12:53:55 -0500

    Updates test variables

--- test/stages/stage2-cloud-operator.tf
+++ test/stages/stage2-cloud-operator.tf
@@ -1,8 +1,8 @@
 module "dev_cloud-operator" {
   source = "./module"
 
-  resource_group_name = module.dev_cluster.resource_group
-  resource_location   = module.dev_cluster.region
+  resource_group_name = var.resource_group_name
+  resource_location   = var.region
   cluster_config_file = module.dev_cluster.config_file_path
   ibmcloud_api_key    = var.ibmcloud_api_key
 }
