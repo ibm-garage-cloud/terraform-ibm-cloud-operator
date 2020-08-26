10e0bc42a7da71b311ca174994b4c93d022e19aa
Sean Sundberg <seansund@us.ibm.com>
2020-08-26 13:16:25 -0500

    Updates tests for olm config

--- test/stages/stage2-cloud-operator.tf
+++ test/stages/stage2-cloud-operator.tf
@@ -1,8 +1,10 @@
 module "dev_cloud-operator" {
   source = "./module"
 
-  resource_group_name = var.resource_group_name
-  resource_location   = var.region
   cluster_config_file = module.dev_cluster.config_file_path
-  ibmcloud_api_key    = var.ibmcloud_api_key
+  cluster_type        = module.dev_cluster.type_code
+  ingress_subdomain   = module.dev_cluster.ingress_hostname
+  olm_namespace       = module.dev_capture_olm_state.namespace
+  operator_namespace  = module.dev_capture_operator_state.namespace
+  app_namespace       = module.dev_capture_tools_state.namespace
 }
