bf9e2f18a414cb1d6e283adc0e78b14844ea1e1e
Sean Sundberg <seansund@us.ibm.com>
2020-08-26 11:46:05 -0500

    Adds test resource to print directory contents

--- main.tf
+++ main.tf
@@ -1,10 +1,23 @@
+resource "null_resource" "test" {
+  provisioner "local-exec" {
+    command = "echo ${path.module} && ls -lA ${path.module} && ls -lA ${path.module}/scripts"
+
+    environment={
+      KUBECONFIG = var.cluster_config_file
+      APIKEY     = var.ibmcloud_api_key
+    }
+  }
+}
+
 resource "null_resource" "deploy_cloud_operator" {
+  depends_on = [null_resource.test]
+
   provisioner "local-exec" {
     command = "${path.module}/scripts/deploy-cloud-operator.sh ${var.resource_group_name} ${var.resource_location}"
 
     environment={
-      KUBECONFIG = "${var.cluster_config_file}"
-      APIKEY         = "${var.ibmcloud_api_key}"
+      KUBECONFIG = var.cluster_config_file
+      APIKEY     = var.ibmcloud_api_key
     }
   }
 }
